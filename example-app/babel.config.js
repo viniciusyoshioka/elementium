c3a19e54ba9a43596a02224e3245f706aad5d071
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-02-19 15:08:26 -0300

    Initial commit

---
+++ example-app/babel.config.js
@@ -0,0 +1,34 @@
+const path = require("path")
+const fs = require("fs")
+
+const packages = path.resolve(__dirname, "..", "packages")
+
+// List all packages under `packages/`
+const workspaces = fs
+    .readdirSync(packages)
+    .map((p) => path.join(packages, p))
+    .filter((p) => fs.statSync(p).isDirectory()
+        && fs.existsSync(path.join(p, "package.json"))
+    )
+
+// Get the aliases for packages `source` field in their `package.json`
+const aliases = {}
+workspaces.forEach((it) => {
+    const pak = JSON.parse(
+        fs.readFileSync(path.join(it, "package.json"), "utf8")
+    )
+    aliases[pak.name] = path.join(it, pak.source)
+})
+
+module.exports = {
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [
+        [
+            "module-resolver",
+            {
+                extensions: [".tsx", ".ts", ".js", ".json"],
+                alias: aliases,
+            },
+        ],
+    ],
+}
---
+++ example-app/metro.config.js
@@ -0,0 +1,66 @@
+const path = require("path")
+const fs = require("fs")
+const escape = require("escape-string-regexp")
+const exclusionList = require("metro-config/src/defaults/exclusionList")
+
+const root = path.resolve(__dirname, "..")
+const packages = path.resolve(root, "packages")
+
+// List all packages under `packages/`
+const workspaces = fs
+    .readdirSync(packages)
+    .map((p) => path.join(packages, p))
+    .filter((p) => fs.statSync(p).isDirectory()
+        && fs.existsSync(path.join(p, "package.json"))
+    )
+
+// Get the list of dependencies for all packages in the monorepo
+const modules = []
+    .concat(...workspaces.map((it) => {
+        const pak = JSON.parse(
+            fs.readFileSync(path.join(it, "package.json"), "utf8")
+        )
+
+        // We need to make sure that only one version is loaded for peerDependencies
+        // So we exclude them at the root, and alias them to the versions in example's node_modules
+        return pak.peerDependencies ? Object.keys(pak.peerDependencies) : []
+    }))
+    .sort()
+    .filter((m, i, self) =>
+        // Remove duplicates and package names of the packages in the monorepo
+        self.lastIndexOf(m) === i && !m.startsWith("@elementium/")
+    )
+
+module.exports = {
+    projectRoot: __dirname,
+    watchFolders: [root],
+
+    // We need to make sure that only one version is loaded for peerDependencies
+    // So we blacklist them at the root, and alias them to the versions in example's node_modules
+    resolver: {
+        blacklistRE: exclusionList([]
+            .concat(...workspaces.map((it) => modules.map((m) =>
+                new RegExp(`^${escape(path.join(it, "node_modules", m))}\\/.*$`)
+            )))
+            .concat(...modules.map((m) =>
+                new RegExp(`^${escape(path.join(root, "node_modules", m))}\\/.*$`)
+            ))
+        ),
+
+        // When we import a package from the monorepo, metro won't be able to find their deps
+        // We need to specify them in `extraNodeModules` to tell metro where to find them
+        extraNodeModules: modules.reduce((acc, name) => {
+            acc[name] = path.join(__dirname, "node_modules", name)
+            return acc
+        }, {}),
+    },
+
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true,
+            },
+        }),
+    },
+}
