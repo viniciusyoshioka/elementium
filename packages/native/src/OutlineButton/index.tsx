c13486b205f43469aa708c3cf8144dc1e7097f71
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-03-19 19:38:39 -0300

    Created OutlineButton in native package

---
+++ packages/native/src/OutlineButton/index.tsx
@@ -0,0 +1,154 @@
+import { Color, Prisma } from "@elementium/color"
+import { useCallback, useMemo, useState } from "react"
+import {
+    ColorValue,
+    GestureResponderEvent,
+    StyleSheet,
+    TextStyle,
+    TouchableOpacity,
+    TouchableOpacityProps,
+    ViewStyle
+} from "react-native"
+
+import { ExtendableOptionalIconProps, Icon, Text, useThemeScheme } from ".."
+import { InteractionStateToken, tokens } from "../tokens"
+
+
+export interface OutlineButtonProps extends
+    Omit<ExtendableOptionalIconProps, "style">,
+    Omit<TouchableOpacityProps, "style">
+{
+    text: string;
+    style?: TextStyle;
+}
+
+
+// TODO add ripple effect
+// TODO add animation for color change
+// TODO add hover state
+// TODO add focus state
+export function OutlineButton(props: OutlineButtonProps) {
+
+
+    const themeScheme = useThemeScheme()
+
+    const backgroundColor = useMemo(() => (
+        props.style?.backgroundColor ?? "transparent"
+    ), [props.style?.backgroundColor])
+    const borderColor = useMemo(() => (
+        props.style?.borderColor ?? "gray"
+    ), [props.style?.borderColor])
+    const color = useMemo(() => {
+        const themeContentColor = themeScheme === "dark" ? "white" : "black"
+        return props.style?.color ?? themeContentColor
+    }, [props.style?.color])
+
+    const [interactionState, setInteractionState] = useState<InteractionStateToken>("enabled")
+
+
+    const contentColor: string | ColorValue = useMemo(() => {
+        if (props.disabled) {
+            return new Color(tokens.color[themeScheme ?? "light"].onSurface)
+                .setA(tokens.stateOpacity.content.disabled)
+                .toRgba()
+        }
+
+        return color
+    }, [props.disabled, themeScheme, color])
+
+    const buttonStyle: ViewStyle = useMemo(() => {
+        const buttonPaddingLeft = props.iconName ? 16 : 24
+        const buttonPaddingRight = props.iconName ? 24 : 24
+        return {
+            ...styles.container,
+            paddingLeft: buttonPaddingLeft,
+            paddingRight: buttonPaddingRight,
+        }
+    }, [props.iconName])
+
+    const buttonColorStyle: ViewStyle = useMemo(() => {
+        const colorStyle: ViewStyle = {
+            borderColor: borderColor,
+            backgroundColor: backgroundColor,
+        }
+
+        if (props.disabled) {
+            const disabledBorderColor = new Color(tokens.color[themeScheme ?? "light"].onSurface)
+                .setA(tokens.stateOpacity.container.disabled)
+                .toRgba()
+
+            colorStyle.borderColor = disabledBorderColor
+            colorStyle.backgroundColor = "transparent"
+        } else if (interactionState === "pressed") {
+            const currentBackgroundColor = new Color(backgroundColor as string)
+            const overlayColor = new Color(contentColor as string).setA(tokens.stateOpacity.container.pressed)
+            const newBackgroundColor = Prisma.addColors(currentBackgroundColor, overlayColor).toRgba()
+
+            colorStyle.backgroundColor = newBackgroundColor
+        }
+
+        return colorStyle
+    }, [borderColor, backgroundColor, props.disabled, themeScheme, interactionState, contentColor])
+
+
+    const ButtonIcon = useCallback(() => {
+        if (!props.iconName) return null
+
+        return (
+            <Icon
+                name={props.iconName}
+                group={props.iconGroup}
+                size={18}
+                style={{
+                    paddingRight: 8,
+                    color: contentColor,
+                }}
+            />
+        )
+    }, [props.iconName, props.iconGroup, contentColor])
+
+
+    function onPressIn(e: GestureResponderEvent) {
+        setInteractionState("pressed")
+        if (props.onPressIn) props.onPressIn(e)
+    }
+
+    function onPressOut(e: GestureResponderEvent) {
+        setInteractionState("enabled")
+        if (props.onPressOut) props.onPressOut(e)
+    }
+
+
+    return (
+        <TouchableOpacity
+            activeOpacity={1}
+            {...props}
+            style={[buttonStyle, props.style, buttonColorStyle]}
+            onPressIn={onPressIn}
+            onPressOut={onPressOut}
+        >
+            <ButtonIcon />
+
+            <Text
+                variant={"label"}
+                size={"large"}
+                style={{ color: contentColor }}
+            >
+                {props.text}
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+        height: 40,
+        maxHeight: 40,
+        borderRadius: 20,
+        borderWidth: 1,
+    },
+})
--- packages/native/src/index.ts
+++ packages/native/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./FilledButton"
 export * from "./Icon"
+export * from "./OutlineButton"
 export * from "./Screen"
 export * from "./Text"
 export * from "./TextButton"
