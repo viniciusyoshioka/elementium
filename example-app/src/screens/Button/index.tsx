b69d7244fd35743cde6e14001ec6c637a88ae817
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-03-19 15:49:33 -0300

    Add FilledButton to Button screen of example-app

--- example-app/src/screens/Button/index.tsx
+++ example-app/src/screens/Button/index.tsx
@@ -1,4 +1,48 @@
+import { FilledButton, Screen, Text } from "@elementium/native"
+
+import { useAppTheme } from "@theme"
+
 
 export function ButtonScreen() {
-    return null
+
+
+    const { color } = useAppTheme()
+
+
+    return (
+        <Screen style={{ margin: 16 }}>
+            <>
+                <Text
+                    variant={"title"}
+                    size={"large"}
+                    style={{ color: color.onBackground, width: "100%" }}
+                    children={"FilledButton"}
+                />
+
+                <FilledButton
+                    text={"Olá, mundo!"}
+                    style={{ marginTop: 16, color: "white" }}
+                />
+
+                <FilledButton
+                    text={"Olá, mundo!"}
+                    iconName={"add"}
+                    style={{ marginTop: 16, color: "white" }}
+                />
+
+                <FilledButton
+                    text={"Olá, mundo!"}
+                    style={{ marginTop: 16, color: "white" }}
+                    disabled
+                />
+
+                <FilledButton
+                    text={"Olá, mundo!"}
+                    iconName={"add"}
+                    style={{ marginTop: 16, color: "white" }}
+                    disabled
+                />
+            </>
+        </Screen>
+    )
 }
