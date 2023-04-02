a44d3364e497d2db113c34644d066ca71445f60a
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-04-02 11:01:34 -0300

    Simplified text in Text screen of example-app

--- example-app/src/screens/Text/index.tsx
+++ example-app/src/screens/Text/index.tsx
@@ -18,138 +18,64 @@ export function TextScreen() {
             contentContainerStyle={{ padding: 16 }}
             onScroll={e => scrollY.value = e.nativeEvent.contentOffset.y}
         >
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"display"} size={"large"} style={{ color: color.onBackground, marginBottom: 8 }}>
                 Display Large
             </Text>
 
-            <Text variant={"display"} size={"large"} style={{ color: color.onBackground, marginBottom: 8 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"display"} size={"medium"} style={{ color: color.onBackground, marginBottom: 8 }}>
                 Display Medium
             </Text>
 
-            <Text variant={"display"} size={"medium"} style={{ color: color.onBackground, marginBottom: 8 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"display"} size={"small"} style={{ color: color.onBackground, marginBottom: 24 }}>
                 Display Small
             </Text>
 
-            <Text variant={"display"} size={"small"} style={{ color: color.onBackground, marginBottom: 24 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"headline"} size={"large"} style={{ color: color.onBackground, marginBottom: 8 }}>
                 Headline Large
             </Text>
 
-            <Text variant={"headline"} size={"large"} style={{ color: color.onBackground, marginBottom: 8 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"headline"} size={"medium"} style={{ color: color.onBackground, marginBottom: 8 }}>
                 Headline Medium
             </Text>
 
-            <Text variant={"headline"} size={"medium"} style={{ color: color.onBackground, marginBottom: 8 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"headline"} size={"small"} style={{ color: color.onBackground, marginBottom: 24 }}>
                 Headline Small
             </Text>
 
-            <Text variant={"headline"} size={"small"} style={{ color: color.onBackground, marginBottom: 24 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"title"} size={"large"} style={{ color: color.onBackground, marginBottom: 8 }}>
                 Title Large
             </Text>
 
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground, marginBottom: 8 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"title"} size={"medium"} style={{ color: color.onBackground, marginBottom: 8 }}>
                 Title Medium
             </Text>
 
-            <Text variant={"title"} size={"medium"} style={{ color: color.onBackground, marginBottom: 8 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"title"} size={"small"} style={{ color: color.onBackground, marginBottom: 24 }}>
                 Title Small
             </Text>
 
-            <Text variant={"title"} size={"small"} style={{ color: color.onBackground, marginBottom: 24 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"body"} size={"large"} style={{ color: color.onBackground, marginBottom: 8 }}>
                 Body Large
             </Text>
 
-            <Text variant={"body"} size={"large"} style={{ color: color.onBackground, marginBottom: 8 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"body"} size={"medium"} style={{ color: color.onBackground, marginBottom: 8 }}>
                 Body Medium
             </Text>
 
-            <Text variant={"body"} size={"medium"} style={{ color: color.onBackground, marginBottom: 8 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"body"} size={"small"} style={{ color: color.onBackground, marginBottom: 24 }}>
                 Body Small
             </Text>
 
-            <Text variant={"body"} size={"small"} style={{ color: color.onBackground, marginBottom: 24 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"label"} size={"large"} style={{ color: color.onBackground, marginBottom: 8 }}>
                 Label Large
             </Text>
 
-            <Text variant={"label"} size={"large"} style={{ color: color.onBackground, marginBottom: 8 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
+            <Text variant={"label"} size={"medium"} style={{ color: color.onBackground, marginBottom: 8 }}>
                 Label Medium
             </Text>
 
-            <Text variant={"label"} size={"medium"} style={{ color: color.onBackground, marginBottom: 8 }}>
-                Lorem ipsum dolor sit amet.
-            </Text>
-
-
-            <Text variant={"title"} size={"large"} style={{ color: color.onBackground }}>
-                Label Small
-            </Text>
-
             <Text variant={"label"} size={"small"} style={{ color: color.onBackground }}>
-                Lorem ipsum dolor sit amet.
+                Label Small
             </Text>
         </ScrollView>
     )
