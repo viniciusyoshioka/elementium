d12fadf2539cf8ae418dc18589188e478966a2e5
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-04-05 17:14:05 -0300

    Add default background color to Screen component

--- packages/native/src/components/Screen/Screen.tsx
+++ packages/native/src/components/Screen/Screen.tsx
@@ -1,14 +1,23 @@
 import { StyleSheet, View, ViewProps } from "react-native"
 
+import { useTheme } from "../../theme"
+
 
 export interface ScreenProps extends ViewProps {}
 
 
 export function Screen(props: ScreenProps) {
+
+    const { color } = useTheme()
+
     return (
         <View
             {...props}
-            style={[styles.screen, props.style]}
+            style={[
+                styles.screen,
+                { backgroundColor: color.background },
+                props.style
+            ]}
         />
     )
 }
