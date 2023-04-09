42d219d3cf97361cf4c980dc46cae29f65179065
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-04-09 14:29:06 -0300

    Created ModalContent component

---
+++ packages/native/src/components/Modal/Content.tsx
@@ -0,0 +1,35 @@
+import { ScrollView, ScrollViewProps, View } from "react-native"
+
+import { Divider } from ".."
+
+
+export interface ModalContentProps extends ScrollViewProps {
+    hasDivider?: boolean;
+}
+
+
+export function ModalContent(props: ModalContentProps) {
+
+
+    const hasDivider = props.hasDivider ?? true
+
+
+    return (
+        <>
+            {hasDivider && <Divider style={{ marginTop: 16 }} />}
+
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                {...props}
+                style={[ { flexGrow: 0 }, props.style]}
+            >
+                <View>
+
+                    {props.children}
+                </View>
+            </ScrollView>
+
+            {hasDivider && <Divider />}
+        </>
+    )
+}
--- packages/native/src/components/Modal/index.ts
+++ packages/native/src/components/Modal/index.ts
@@ -1,5 +1,6 @@
 export * from "./Actions"
 export * from "./Container"
+export * from "./Content"
 export * from "./Description"
 export * from "./Icon"
 export * from "./Modal"
