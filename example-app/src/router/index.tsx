bce6e8c60bb9ef5b5642944db81dfd13ae0e7112
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-10-21 01:46:41 -0300

    Delete ListItem from example-app

--- example-app/src/router/index.tsx
+++ example-app/src/router/index.tsx
@@ -5,7 +5,6 @@ import { Pressable, StatusBar } from "react-native"
 
 import { IconScreen } from "@screens/Icon"
 import { InputScreen } from "@screens/Input"
-import { ListItemScreen } from "@screens/ListItem"
 import { ModalScreen } from "@screens/Modal"
 import { RadioButtonScreen } from "@screens/RadioButton"
 import { useAppTheme } from "@theme"
@@ -52,7 +51,6 @@ export function Router() {
             >
                 <Drawer.Screen name={"Icon"} component={IconScreen} />
                 <Drawer.Screen name={"Input"} component={InputScreen} />
-                <Drawer.Screen name={"ListItem"} component={ListItemScreen} />
                 <Drawer.Screen name={"Modal"} component={ModalScreen} />
                 <Drawer.Screen name={"RadioButton"} component={RadioButtonScreen} />
             </Drawer.Navigator>
