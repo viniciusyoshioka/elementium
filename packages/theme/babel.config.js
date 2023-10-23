9f14f56450994814625d0986ff8262dee6131574
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-10-22 22:55:44 -0300

    Removed metro-react-native-babel-preset from theme package to make it independent of react native

--- packages/theme/babel.config.js
+++ packages/theme/babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-    presets: ["module:metro-react-native-babel-preset"],
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                "modules": false
+            }
+        ],
+        "@babel/preset-typescript"
+    ],
     plugins: [
         "@babel/plugin-proposal-export-namespace-from",
     ],
